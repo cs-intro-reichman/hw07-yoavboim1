02e27b0fe4f97ac4c3138ee2338d57d9adb981b7
yoavboim <yoavboim1@gmail.com>
2025-12-28 20:14:13 +0200

    to binary + palindrome  tested

--- IntToBin.java
+++ IntToBin.java
@@ -2,13 +2,25 @@
 public class IntToBin {
 
     public static void main(String[] args) {
-        int x = Integer.parseInt(args[0]);
-        System.out.println("Binary representation of " + x + " is: " + toBinary(x));
+        int number = Integer.parseInt(args[0]);
+        System.out.println("Binary representation of " + number + " is: " + toBinary(number));
     }
 
-    /** Returns the binary representation of the given integer, as a string of 0's and 1's.  */
-    public static String toBinary(int x) {
-        //// Replace the following statement with your code
-        return "";
+    /** Returns the binary representation of the given integer, as a string of 0's and 1's. */
+    public static String toBinary(int number) {
+        String binaryStr = "0";
+        
+        if (number == 0)
+            return "0";
+        
+        if (number == 1)
+            return "1";
+        
+        if (number % 2 == 0)
+            binaryStr = toBinary(number / 2) + '0';
+        else
+            binaryStr = toBinary(number / 2) + '1';
+            
+        return binaryStr;
     }    
- }
+}
--- Palindrome.java
+++ Palindrome.java
@@ -1,13 +1,18 @@
 /** Checks if a given string is a palindrome. */
 public class Palindrome {
 
-	public static void main(String[]args) {
-    	System.out.println(isPalindrome(args[0]));
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(args[0]));
     }
-	
-	/** Checks if the given string is a palindrome. */
-	public static boolean isPalindrome(String s) {
-		//// Replace the followaing statement with your code
-		return false;
+    
+    /** Checks if the given string is a palindrome. */
+    public static boolean isPalindrome(String str) {
+        if (str.length() <= 1)
+            return true;
+
+        if (str.charAt(str.length() - 1) != str.charAt(0))
+            return false;
+
+        return isPalindrome(str.substring(1, str.length() - 1));
     }
 }
